3dc53e5675b9248ff33cc5b8b79afc3a7426b4bb
zhanghui <zhanghui026@gmail.com>
2020-04-14 08:00:27 +0800

    add painting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Admin, Resource, ShowGuesser, EditGuesser, fetchUtils } from 'react-admin';
-import { UserList } from './user';
+import {Admin, Resource, ShowGuesser, EditGuesser, fetchUtils} from 'react-admin';
+import {UserList} from './user';
 import authProvider from './authProvider';
-import { PostList, PostEdit, PostCreate } from './posts';
+import {PostList, PostEdit, PostCreate} from './posts';
 // import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
@@ -11,8 +11,7 @@ import dataProvider from './myDataProvider';
 import MyLogoutButton from './MyLogoutButton';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from './i18n/en';
-import Dashboard  from './dashboard/Dashboard';
-
+import Dashboard from './dashboard/Dashboard';
 
 
 import orders from './orders';
@@ -21,7 +20,7 @@ import categories from './categories';
 import visitors from './visitors';
 import invoices from './invoices'
 import reviews from './reviews';
-
+import paintings from './paintings';
 
 
 // import MyLoginPage from './MyLoginPage';
@@ -31,14 +30,13 @@ import Login from './layout/Login'
 const apiUrl = 'http://localhost:8080/api';
 
 
-
 const httpClient = (url, options = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({ Accept: 'application/json' });
-  }
-  const raToken = localStorage.getItem('raToken');
-  options.headers.set('Authorization', `${raToken}`);
-  return fetchUtils.fetchJson(url, options);
+    if (!options.headers) {
+        options.headers = new Headers({Accept: 'application/json'});
+    }
+    const raToken = localStorage.getItem('raToken');
+    options.headers.set('Authorization', `${raToken}`);
+    return fetchUtils.fetchJson(url, options);
 };
 
 const dataP = dataProvider(apiUrl, httpClient);
@@ -47,39 +45,40 @@ const authP = authProvider(apiUrl);
 
 
 const i18nProvider = polyglotI18nProvider(locale => {
-  // if (locale === 'fr') {
-  //     return import('./i18n/fr').then(messages => messages.default);
-  // }
+    // if (locale === 'fr') {
+    //     return import('./i18n/fr').then(messages => messages.default);
+    // }
 
-  // Always fallback on english
-  return englishMessages;
+    // Always fallback on english
+    return englishMessages;
 }, 'en');
 
 const App = () => (
-  <Admin
-    loginPage={Login}
-    dashboard={Dashboard}
-    authProvider={authP}
-    dataProvider={dataP}
-    logoutButton={MyLogoutButton}
-    i18nProvider={i18nProvider}>
-    {permissions => [
-      // <Resource name="posts" list={PostList} icon={PostIcon} create={PostCreate} edit={PostEdit} show={ShowGuesser} />,
-      // <Resource name="clients" list={UserList} show={ShowGuesser} edit={EditGuesser} icon={UserIcon}></Resource>,
-      <Resource name="customers" {...visitors} />,
-      <Resource
-        name="commands"
-        {...orders}
-        options={{ label: 'Orders' }}
-      />,
+    <Admin
+        loginPage={Login}
+        dashboard={Dashboard}
+        authProvider={authP}
+        dataProvider={dataP}
+        logoutButton={MyLogoutButton}
+        i18nProvider={i18nProvider}>
+        {permissions => [
+            // <Resource name="posts" list={PostList} icon={PostIcon} create={PostCreate} edit={PostEdit} show={ShowGuesser} />,
+            // <Resource name="clients" list={UserList} show={ShowGuesser} edit={EditGuesser} icon={UserIcon}></Resource>,
+            <Resource name="customers" {...visitors} />,
+            <Resource
+                name="commands"
+                {...orders}
+                options={{label: 'Orders'}}
+            />,
 
-      <Resource name="products" {...products} />,
-      <Resource name="categories" {...categories} />,
-      <Resource name="invoices" {...invoices} />,
-      <Resource name="reviews" {...reviews} />,
+            <Resource name="products" {...products} />,
+            <Resource name="categories" {...categories} />,
+            <Resource name="invoices" {...invoices} />,
+            <Resource name="reviews" {...reviews} />,
+            <Resource name="paintings" {...paintings}/>,
 
-    ]}
-  </Admin>
+        ]}
+    </Admin>
 );
 
 export default App;
---
+++ src/paintings/index.js
@@ -0,0 +1,9 @@
+import PaintingIcon from '@material-ui/icons/Collections';
+import PaintingList from './PaintingList';
+// import ProductEdit from './ProductEdit';
+// import ProductCreate from './ProductCreate';
+
+export default {
+    list: PaintingList,
+    icon: PaintingIcon,
+};
